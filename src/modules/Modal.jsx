ac12e7b020f4ebdc380b9a238aa0782a260e3a00
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-03-27 09:04:07 +0100

    set base for modal

---
+++ src/modules/Modal.jsx
@@ -0,0 +1,28 @@
+/* HOW TO HANTERA MODALEN!:
+
+- Skapa en variabel med de innehåll som modalen ska visa
+  Exempel: 
+    const variabelFörmodalContent =
+    <>
+      <h1>Här är modal rubrik</h1>
+      <h2>Här är underrubrik</h2>
+      <p>... och så lite brödtext Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque nihil accusantium accusamus doloremque ea vitae rem debitis recusandae sint, fuga eos hic sed ab ipsam labore dicta, odio nostrum iure.</p>
+      <button>Knapp</button>
+    </>;
+
+- Kalla på modalen och skicka in variabeln som prop "modalContent".
+  Exempel:
+    <Modal modalContent={variabelFörmodalContent} />
+*/
+
+
+
+export default function Modal(props) {
+
+    return (
+        <>
+            {props.modalContent}
+        </>
+
+    )
+};
