1470112125b3f62c98f82247d8baa24ee834ef8a
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-03-30 16:42:32 +0100

    card is saving now

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ColumnsContainer from './modules/ColumnsContainer'
 import Modal from './modules/Modal'
 import { useEffect, useState, createContext } from 'react'
 
+
 import { getTasks, saveTasks } from './utils/ApiUtils'
 
 //KONTEXT FÖR KOLUMNER
@@ -41,49 +42,11 @@ function App() {
   ]);
 
   // Värde för tasks
-  const [tasks, setTasks] = useState([{
-    title: "Vattna Blommorna",
-    id: 1,
-    parentColumnId: 1,
-    description: "Det behövs innan dom dör",
-    deadline: "NUUUUUU",
-    timeStampCreated: "igår",
-    timeStampLastEdited: "nu",
-    timeStampLastMoved: null,
-  }, {
-    title: "Slänga soporna",
-    id: 2,
-    parentColumnId: 1,
-    description: "Glöm inte att porten till soprummet är tung att öppna!",
-    deadline: null,
-    timeStampCreated: "förra veckan",
-    timeStampLastEdited: "igår",
-    timeStampLastMoved: null,
-  }, {
-    title: "Kolla på TV",
-    id: 3,
-    parentColumnId: 2,
-    description: "Viktigt att få tid för sig själv också ju!",
-    deadline: null,
-    timeStampCreated: "idag",
-    timeStampLastEdited: null,
-    timeStampLastMoved: null,
-  }, {
-    title: "Bygga en ny app",
-    id: 4,
-    parentColumnId: 3,
-    description: "Bygg dendär appen som håller koll på uppgifter jag har att göra!",
-    deadline: "igår",
-    timeStampCreated: "två veckor sedan",
-    timeStampLastEdited: "igår",
-    timeStampLastMoved: null,
-  }]);
+  const [tasks, setTasks] = useState(getTasks());
+  console.log(tasks);
 
 
-  useEffect(() => {
-    setTasks(getTasks());
-    saveTasks(tasks)
-  }, [])
+
 
   return (
     <div className='app_container'>
