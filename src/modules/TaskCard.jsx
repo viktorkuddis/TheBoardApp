5de4d68a485d2fbc2bf87d8143a99121bb315a94
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-04-03 15:54:56 +0200

    drop down exist on every card

--- src/modules/TaskCard.jsx
+++ src/modules/TaskCard.jsx
@@ -1,6 +1,7 @@
 
 import { useContext, useState } from "react";
 import { tasksContext } from "../App";
+import { columnsContext } from "../App";
 
 import AdvancedEditModal from "./AdvancedEditModal";
 
@@ -8,13 +9,21 @@ import AdvancedEditModal from "./AdvancedEditModal";
 export default function TaskCard({ task, markedAsDone, columnColor }) {
 
 
+    const { columns, setColumns } = useContext(columnsContext)
+
     //Variabel som togglar om modalen för avancerad edit ska vara öppen eller ej
     const [advancedEditisOpend, setadvancedEditisOpend] = useState(false);
 
     //funktoin som hanterar då ett kort klickas för att öppna redigeringsvy.
-    function handleCardClick() {
-        setadvancedEditisOpend(true);
-        console.log("klick på kort")
+    function handleCardClick(e) {
+        if (e.target.nodeName !== "SELECT") {
+            setadvancedEditisOpend(true);
+            console.log("klick på kort")
+
+        }
+
+        console.log(e)
+
     }
 
     return (
@@ -33,7 +42,14 @@ export default function TaskCard({ task, markedAsDone, columnColor }) {
                 {task.deadline && typeof task.deadline === 'string' && task.deadline.trim() !== "" &&
                     <p style={{ fontSize: "0.8rem", color: "grey" }}>
                         Deadline: {task.deadline}</p>}
+                <select name="Flytta Uppgift" defaultValue="Flytta">
+                    {/* generera option för varje kolumn som finns:*/}
+                    <option value="Flytta" disabled>Flytta...</option>
+                    {columns.map((column) => {
+                        return <option key={column.columnID} value={column.columnID}>{column.columnName}</option>
+                    })}
 
+                </select>
 
 
 
