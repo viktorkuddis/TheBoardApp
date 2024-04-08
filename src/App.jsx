59baa7f03e20edc6ebe6b49d7b5d0490ce422c16
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-04-08 10:50:38 +0200

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './AddTaskCard.css'
 import './Animations.css'
 import './ColumnsSettingsModal.css'
 
-import { useEffect, useState, createContext } from 'react'
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //Sidor:
@@ -16,63 +14,21 @@ import OuupsPage from './Pages/OuupsPage'
 //layoutfil:
 import Layout from './Pages/Layout'
 
-
+//Kontext providers:
 import { TasksContextProvider } from './context/TasksContext'
 import { ColumnsContextProvider } from './context/ColumnsContext';
 
 
-//? KONTEXT FÖR KOLUMNER
-//?  export const columnsContext = createContext();
 
-// !KONTEXT FÖR TASKS
-// ! export const tasksContext = createContext();
-
-// !import { getTasks, saveTasks } from './utils/ApiUtils'
-//? import { getColumns, saveColumns } from './utils/ApiUtils'
 
 function App() {
-
-
-  // ?Värde för columnContext
-  //? const [columns, setColumns] = useState(getColumns());
-  //? console.log(columns)
-
-  // !Värde för tasks
-  // !const [tasks, setTasks] = useState(getTasks());
-  // !console.log(tasks);
-
-  //? Spara columns när variabeln columns uppdateras:
-  //? useEffect(() => {
-  //? saveColumns(columns);
-  //? }, [columns])
-
-  // !Spara tasks när variabeln tasks uppdateras:
-  // !useEffect(() => {
-  // !  saveTasks(tasks);
-  // !}, [tasks])
-
-  //?hanterar state för modalen som redigerar kolumner
-  // ?const [showColumnSettingsModal, setShowColumnSettingsModal] = useState(false)
-  //?håller id på den kolumn som anändaren vill redigera:
-  //? const [columnToEdit, setColumnToEdit] = useState(false);
 
 
   return (
 
     <div className='app_container'>
 
-
-
-      {/* //?<columnsContext.Provider
-        value={{
-          columns, setColumns,
-          showColumnSettingsModal, setShowColumnSettingsModal, columnToEdit, setColumnToEdit
-        }}> */}
-
-      {/* //! <tasksContext.Provider value={{ tasks, setTasks }}> */}
-
       <ColumnsContextProvider>
-
         <TasksContextProvider>
 
           <Router>
@@ -88,11 +44,6 @@ function App() {
 
         </TasksContextProvider>
       </ColumnsContextProvider>
-
-      {/* //! </tasksContext.Provider> */}
-      {/* //? </columnsContext.Provider> */}
-
-
 
     </div >
 
